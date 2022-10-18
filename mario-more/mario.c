fbb863cc29b52a5625f281c927f3f3d12a52197f
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-10-18 22:22:17 +0000

    Tue, Oct 18, 2022, 10:22 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+string str(int i) 
+
+int main(void)
 {
-    
+    int height;
+    do{
+        height = get_int("Height: ");
+    }
+    while(height  < 1 || height > 8);
+    for(int i = 1; i <= height; i++){
+
+    }
 }
