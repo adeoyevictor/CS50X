9cd09c221dfd5d1dd1609231b2452b97c3a5804d
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-10-19 22:34:33 +0000

    Wed, Oct 19, 2022, 10:34 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,53 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int sumDigits(int n)
 {
-    
+
+}
+
+int main(void)
+{
+    long num = get_long("Number: ");
+    int i = 2;
+    int multiplied = 0;
+    int notMultiplied = 0;
+
+    while(true)
+    {
+         if(num<10)
+         {
+            // do something
+            break;
+         }
+
+        int digit = num % 10;
+        num = num/10;
+
+        if(i % 2 == 1)
+        {
+            digit = digit * 2;
+            if(digit > 10)
+            {
+                digit = sumDigits(digit);
+            }
+            multiplied += digit;
+        }
+        else if(i % 2 == 0){
+            if(digit > 10)
+            {
+                digit = sumDigits(digit);
+            }
+            notMultiplied += digit;
+        }
+
+
+    }
+    int sum = mutiplied + notMultiplied;
+    if(sum % 10 == 0){
+
+    }
+    else{
+        printf("INVALID")
+    }
 }
