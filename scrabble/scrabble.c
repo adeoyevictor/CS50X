955e4f32908fb696b277bb56cf98872a357d24d4
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-10-29 17:48:49 +0000

    Sat, Oct 29, 2022, 5:48 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,6 +19,18 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+    if(score1 > score2)
+    {
+        printf("Player 1 wins!");
+    }
+    else if(score2 > score1)
+    {
+        printf("Player 2 wins!");
+    }
+    else
+    {
+        printf("Tie!");
+    }
 }
 
 int compute_score(string word)
@@ -38,7 +50,8 @@ int compute_score(string word)
         }
         else
         {
-            points += 0
+            points += 0;
         }
     }
+    return points;
 }
