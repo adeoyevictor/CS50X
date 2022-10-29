8e356962b963256b7b1b08274ea73ad75e3d910e
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-10-29 17:36:42 +0000

    Sat, Oct 29, 2022, 5:36 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -24,8 +24,16 @@ int main(void)
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
+    int points = 0;
     for(int i = 0 , n = strlen(word); i < n ; i++)
     {
-        
+        if()
+        {
+
+        }
+        else
+        {
+            
+        }
     }
 }
