91a8b56f89b1527367dd25f6a8635624bddd8a01
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-10-30 21:25:55 +0000

    Sun, Oct 30, 2022, 9:25 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,24 +5,32 @@
 
 int count_letters(string text);
 int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
-    int sentences = 0;
     string text = get_string("Text: ");
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
 
-
-        else if (text[i] == '.' || text[i] == '?' || text[i] == '!')
-        {
-            sentences++;
-        }
-
-
-    printf("%i %i %i", letters, words, sentences);
+    
 }
 
 
 
+int count_sentences(string text)
+{
+     int sentences = 0;
+     for (int i = 0, n = strlen(text); i < n; i++)
+     {
+        if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+        {
+            sentences++;
+        }
+     }
+     return sentences;
+}
 
 
 
