e125ab603b7721d763fbf57e982eec4f4fae4e51
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-10-30 21:02:59 +0000

    Sun, Oct 30, 2022, 9:02 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(void) 
+int main(void)
 {
-
+    int letters;
+    int words;
+    int sentences;
+    string text = get_string("Text: ");
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if(text[i] >= 'a' )
+    }
 }
