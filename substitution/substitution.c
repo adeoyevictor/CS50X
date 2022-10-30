829f98ac895758e388ceb3ef227223b0fa91c7d7
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-10-30 22:36:13 +0000

    Sun, Oct 30, 2022, 10:36 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,11 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
     if(argc != 2)
     {
         printf("Usage: ./substitution key\n");
+        return 1;
+    }
+
+    if (strlen(argv[0]) != 26){
+        printf("");
         return 1;
     }
 }
