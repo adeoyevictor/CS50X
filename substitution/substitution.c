99ba41b48b5ba5ca2dbcd97446a53ff952c8a22c
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-10-31 19:09:04 +0000

    Mon, Oct 31, 2022, 7:09 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -40,7 +40,7 @@ int main(int argc, string argv[])
     }
 
     string plain = get_string("plaintext: ");
-    char cypher[strlen(argv[1])];
+    char cypher[strlen(plain)];
 
     for (int i = 0, n = strlen(plain); i < n; i++ )
     {
