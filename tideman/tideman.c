b4f5cb4fe7cd1799d7e9ba16e22a449bb51ffdf5
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-06 16:33:41 +0000

    Sun, Nov 6, 2022, 4:33 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -132,9 +132,16 @@ void add_pairs(void)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = i + 1; j < candidate_count; j++)
+        for (int j = 0; j < candidate_count; j++)
         {
-            preferences[ranks[i]][ranks[j]]++;
+            if(preferences[i][j] == preferences[j][i])
+            {
+
+            }
+            else
+            {
+                
+            }
         }
     }
     return;
