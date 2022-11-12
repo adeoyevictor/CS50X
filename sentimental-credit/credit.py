9ba2fdd2ca604b35112e3f2eb95e335cb3521dd0
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-12 11:35:19 +0000

    Sat, Nov 12, 2022, 11:35 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,8 +3,11 @@ from cs50 import get_int
 
 def main():
     num = get_int("Number: ")
-    noOfDigits, multiplied, notMultiplied, firstNumber, secondNumber, i = 0,0,0,0,0,0
-    print(noOfDigits, multiplied, notMultiplied, firstNumber, secondNumber, i)
+    noOfDigits, multiplied, notMultiplied, firstNumber, secondNumber, i = 0, 0, 0, 0, 0, 0
+    while True:
+        noOfDigits += 1
+        if num < 10:
+            firstNumber = num
 
 
 
