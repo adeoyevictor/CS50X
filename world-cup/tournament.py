4c9dfe30470bcbe17718b41e8ffafd85ca904334
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-12 09:28:53 +0000

    Sat, Nov 12, 2022, 9:28 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -58,7 +58,7 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-    
+    for 
 
 
 if __name__ == "__main__":
