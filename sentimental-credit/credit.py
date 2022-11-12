60fdf7000c9590864c96036d17268764c5f871f7
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-12 11:29:53 +0000

    Sat, Nov 12, 2022, 11:29 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,12 +3,7 @@ from cs50 import get_int
 
 def main():
     num = get_int("Number: ")
-    noOfDigits = 0
-    multiplied = 0
-    notMultiplied = 0
-    firstNumber = 0
-    secondNumber = 0
-    i = 0
+    noOfDigits, multiplied, notMultiplied, firstNumber, secondNumber, i = 0
 
 
 def sumDigits(n):
