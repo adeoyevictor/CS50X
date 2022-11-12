1270096051dacdc05775ed8ab6cf483860e1c533
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-12 13:21:25 +0000

    Sat, Nov 12, 2022, 1:21 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,1 +1,8 @@
-# TODO
+import from cs50 get_string
+
+def main():
+    text = get_string("Text: ")
+    letters = count_letter(text)
+    words = count_words(text)
+    sentences = count_sentences(text)
+    index = calc_index(letters, words, sentences)
