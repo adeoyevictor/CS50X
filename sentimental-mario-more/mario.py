bf73fd0ee9dd2c22c160bc8c6926bb46ef6e57c7
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-12 10:20:44 +0000

    Sat, Nov 12, 2022, 10:20 AM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,9 +1,14 @@
 # TODO
 from cs50 import get_int
 
-height = None
+# height = None
+def get_height():
+    while True:
+        height = get_int("Height: ")
+        if height in range(1, 9):
+            break
+    return height
 
-while True:
-    height = get_int("Height: ")
-    if height in range(1, height+1):
-        break
+n = get_height()
+
+print(n)
