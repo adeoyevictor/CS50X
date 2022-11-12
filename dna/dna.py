373577acb8bca1349c28126b00358509518033e8
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-12 16:26:03 +0000

    Sat, Nov 12, 2022, 4:26 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -11,12 +11,14 @@ def main():
 
     # TODO: Read database file into a variable
     databs = []
-    
-    with open(sys.argv[1], "r") as file:
-        reader = csv.reader(file)
-        # next(reader)
-        # for row in reader:
 
+    with open(sys.argv[1], "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            databs += [{
+                # "team": row["team"],
+            }]
+    print(databs)
 
 
     # TODO: Read DNA sequence file into a variable
