36706ac2af1dc400a23bf70e2d74a88a26526a11
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-12 11:41:01 +0000

    Sat, Nov 12, 2022, 11:41 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -8,11 +8,24 @@ def main():
         noOfDigits += 1
         if num < 10:
             firstNumber = num
+            if i % 2 == 1:
+                num = num * 2
+                if num > 9:
+                    num = sumDigits(num)
+                multiplied += num
+            elif i % 2 == 0:
+                notMultiplied += num
+            break
+        if num < 100:
+            secondNumber = num % 10
+        digit = num % 10
+        num = num // 10
+
 
 
 
 
 def sumDigits(n):
-    return ( n % 10) + ( n / 10)
+    return ( n % 10) + ( n // 10)
 
 main()
