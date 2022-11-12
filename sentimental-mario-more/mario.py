cee291b227fc4a634a511d61fff353e77d878caf
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-12 11:06:26 +0000

    Sat, Nov 12, 2022, 11:06 AM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,30 +1,26 @@
 # TODO
 from cs50 import get_int
-
-n = 0
-
-def main():
-    n = get_height()
-    draw(n)
-
-def get_height():
-    while True:
-        height = get_int("Height: ")
-        if height in range(1, 9):
-            break
-    return height
-
-
-
-def draw(height):
-    if height < 1:
+def draw(n):
+    if n < 1:
         return
-    draw(height - 1)
+    draw(n - 1)
 
-    print(' ' * (height), end='')
-    print('#' * height, end='')
+    print(' ' * (n), end='')
+    print('#' * n, end='')
     print(' ' * 2, end='')
-    print('#' * height)
+    print('#' * n)
+
+while True:
+    height = get_int("Height: ")
+    if height in range(1, 9):
+        break
+draw(height)
 
 
-main()
+
+
+
+
+
+
+
