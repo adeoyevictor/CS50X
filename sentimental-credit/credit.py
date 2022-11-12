f5df22b95259e812c205a12f089898e6998e9251
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-12 11:57:51 +0000

    Sat, Nov 12, 2022, 11:57 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,6 +3,11 @@ from cs50 import get_int
 
 def main():
     num = get_int("Number: ")
+
+
+
+
+    
     noOfDigits, multiplied, notMultiplied, firstNumber, secondNumber, i = 0, 0, 0, 0, 0, 0
     while True:
         noOfDigits += 1
