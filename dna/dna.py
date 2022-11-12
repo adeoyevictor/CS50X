bee3b5f6cc220632b45cf014e5d1a1d459eb6620
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-12 16:16:05 +0000

    Sat, Nov 12, 2022, 4:16 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -15,7 +15,7 @@ def main():
         reader = csv.reader(file)
         next(reader)
         for row in reader:
-            print(row)
+            print(row[0])
 
     # TODO: Read DNA sequence file into a variable
 
