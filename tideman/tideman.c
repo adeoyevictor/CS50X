df1c7f9846c65b92c1be277249e4c10f913dcc37
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-28 00:54:31 +0000

    Mon, Nov 28, 2022, 12:54 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -223,7 +223,10 @@ void print_winner(void)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            
+            if (!locked[j][i])
+            {
+                
+            }
         }
     }
     return;
