3f1a2455463dfcbe67e6f519e41881c9c272b15d
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-29 23:42:38 +0000

    Tue, Nov 29, 2022, 11:42 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,9 +34,6 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    uint8_t header[HEADER_SIZE] = input;
-
-    size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
 
     // TODO: Read samples from input file and write updated data to output file
 
