7711624fe6b6821d1845e3fcc3c5e33cd59012e5
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-29 23:54:59 +0000

    Tue, Nov 29, 2022, 11:54 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,10 +34,11 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    uint8_t header[HEADER_SIZE] = input;
+    uint8_t header[] = malloc(HEADER_SIZE)
+    header = input;
     for (int i = 0 ; i < HEADER_SIZE; i++)
     {
-        
+
     }
 
     // TODO: Read samples from input file and write updated data to output file
