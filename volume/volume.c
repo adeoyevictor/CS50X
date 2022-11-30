0663cead461f80172b8f65a34ef59c5bba575792
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-30 00:21:52 +0000

    Wed, Nov 30, 2022, 12:21 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -42,6 +42,8 @@ int main(int argc, char *argv[])
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
 
+
+    for (int i = 0; )
     // Close files
     fclose(input);
     fclose(output);
