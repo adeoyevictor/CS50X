ab94f6c56c710efd2ef90a823aa2c54f5081abaf
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-30 00:31:24 +0000

    Wed, Nov 30, 2022, 12:31 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -42,8 +42,8 @@ int main(int argc, char *argv[])
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
 
-    fread(&buffer, sizeof(int16_t), 1, input)
-    fwrite()
+    fread(&buffer, sizeof(int16_t), 1, input);
+    fwrite(&buffer, sizeof(int16_t), 1 , output);
 
 
 
