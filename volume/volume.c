c703f6b8011f724bb4cf495d7c7a3fcfe65d76c9
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-30 00:50:31 +0000

    Wed, Nov 30, 2022, 12:50 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
 
     int16_t buffer;
 
-    while(fread(&buffer, sizeof(int16_t), 1, input))
+    while (fread(&buffer, sizeof(int16_t), 1, input))
     {
         buffer *= factor;
         fwrite(&buffer, sizeof(int16_t), 1, output);
