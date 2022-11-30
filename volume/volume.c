7906f22e3db5e0e802203bcf59651087101c6d6e
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-11-30 00:17:43 +0000

    Wed, Nov 30, 2022, 12:17 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -37,8 +37,8 @@ int main(int argc, char *argv[])
 
     uint8_t header[HEADER_SIZE];
 
-    fread(header, sizeof(uint8_t), HEADER_SIZE, input)
-    fwrite(header, sizeof(uint8_t), HEADER_SIZE, output )
+    fread(header, sizeof(uint8_t), HEADER_SIZE, input);
+    fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
 
