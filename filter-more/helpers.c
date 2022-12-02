ca05e04cf25a338797667522d9de05e3a2128ff8
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-02 23:07:34 +0000

    Fri, Dec 2, 2022, 11:07 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -140,36 +140,52 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             else if (i == 0 && j == width - 1)
             {
                  gx_blue += (image[i][j - 1].rgbtBlue * -2)  +  (image[i + 1][j - 1].rgbtBlue * - 1);
-                 gx_green += (image[i][j - 1].rgbtBlue * -2)  +  (image[i + 1][j - 1].rgbtBlue * - 1);
-                 gx_red += (image[i][j - 1].rgbtBlue * -2)  +  (image[i + 1][j - 1].rgbtBlue * - 1);
+                 gx_green += (image[i][j - 1].rgbtGreen * -2)  +  (image[i + 1][j - 1].rgbtGreen * - 1);
+                 gx_red += (image[i][j - 1].rgbtRed * -2)  +  (image[i + 1][j - 1].rgbtRed * - 1);
             }
             else if (i == height - 1 && j == 0)
             {
                 gx_blue +=  (image[i][j + 1].rgbtBlue * 2)  + (image[i - 1][j + 1].rgbtBlue * 1);
+                gx_green +=  (image[i][j + 1].rgbtGreen * 2)  + (image[i - 1][j + 1].rgbtGreen * 1);
+                gx_red +=  (image[i][j + 1].rgbtRed * 2)  + (image[i - 1][j + 1].rgbtRed * 1);
             }
             else if (i == height - 1 && j == width - 1)
             {
                 gx_blue +=  (image[i][j - 1].rgbtBlue * -2) +  (image[i - 1][j - 1].rgbtBlue * -1);
+                gx_green +=  (image[i][j - 1].rgbtGreen * -2) +  (image[i - 1][j - 1].rgbtGreen * -1);
+                gx_red +=  (image[i][j - 1].rgbtRed * -2) +  (image[i - 1][j - 1].rgbtRed * -1);
             }
             else if (i == 0)
             {
                   gx_blue += (image[i][j - 1].rgbtBlue * -2) + (image[i][j + 1].rgbtBlue * 2) +  (image[i + 1][j - 1].rgbtBlue * -1)  + (image[i + 1][j + 1].rgbtBlue * 1);
+                  gx_green += (image[i][j - 1].rgbtGreen * -2) + (image[i][j + 1].rgbtGreen * 2) +  (image[i + 1][j - 1].rgbtGreen * -1)  + (image[i + 1][j + 1].rgbtGreen * 1);
+                  gx_red += (image[i][j - 1].rgbtRed * -2) + (image[i][j + 1].rgbtRed * 2) +  (image[i + 1][j - 1].rgbtRed * -1)  + (image[i + 1][j + 1].rgbtRed * 1);
             }
             else if (j == 0)
             {
                  gx_blue +=  (image[i][j + 1].rgbtBlue * 2)  + (image[i - 1][j + 1].rgbtBlue * 1) + (image[i + 1][j + 1].rgbtBlue * 1);
+                 gx_green +=  (image[i][j + 1].rgbtGreen * 2)  + (image[i - 1][j + 1].rgbtGreen * 1) + (image[i + 1][j + 1].rgbtGreen * 1);
+                 gx_red +=  (image[i][j + 1].rgbtRed * 2)  + (image[i - 1][j + 1].rgbtRed * 1) + (image[i + 1][j + 1].rgbtRed * 1);
             }
             else if (i == height - 1)
             {
                 gx_blue += (image[i][j - 1].rgbtBlue * -2) + (image[i][j + 1].rgbtBlue * 2) + (image[i - 1][j - 1].rgbtBlue * -1) + (image[i - 1][j + 1].rgbtBlue * 1);
+                gx_green += (image[i][j - 1].rgbtGreen * -2) + (image[i][j + 1].rgbtGreen * 2) + (image[i - 1][j - 1].rgbtGreen * -1) + (image[i - 1][j + 1].rgbtGreen * 1);
+                gx_red += (image[i][j - 1].rgbtRed * -2) + (image[i][j + 1].rgbtRed * 2) + (image[i - 1][j - 1].rgbtRed * -1) + (image[i - 1][j + 1].rgbtRed * 1);
             }
             else if (j == width - 1 )
             {
                 gx_blue +=  (image[i][j - 1].rgbtBlue * -2) + (image[i - 1][j - 1].rgbtBlue * -1) + (image[i + 1][j - 1].rgbtBlue * -1);
+                gx_green +=  (image[i][j - 1].rgbtGreen * -2) + (image[i - 1][j - 1].rgbtGreen * -1) + (image[i + 1][j - 1].rgbtGreen * -1);
+                gx_red +=  (image[i][j - 1].rgbtRed * -2) + (image[i - 1][j - 1].rgbtRed * -1) + (image[i + 1][j - 1].rgbtRed * -1);
             }
             else
             {
                 gx_blue += (image[i][j - 1].rgbtBlue * -2) + (image[i][j + 1].rgbtBlue * 2) + (image[i - 1][j - 1].rgbtBlue * -1) + (image[i + 1][j - 1].rgbtBlue * -1) + (image[i - 1][j + 1].rgbtBlue * 1) + (image[i + 1][j + 1].rgbtBlue * 1);
+
+                gx_green += (image[i][j - 1].rgbtGreen * -2) + (image[i][j + 1].rgbtGreen * 2) + (image[i - 1][j - 1].rgbtGreen * -1) + (image[i + 1][j - 1].rgbtGreen * -1) + (image[i - 1][j + 1].rgbtGreen * 1) + (image[i + 1][j + 1].rgbtGreen * 1);
+
+                gx_red += (image[i][j - 1].rgbtRed * -2) + (image[i][j + 1].rgbtRed * 2) + (image[i - 1][j - 1].rgbtRed * -1) + (image[i + 1][j - 1].rgbtRed * -1) + (image[i - 1][j + 1].rgbtRed * 1) + (image[i + 1][j + 1].rgbtRed * 1);
             }
         }
     }
