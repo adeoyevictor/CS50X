9f78958672a2722f05ca344b7c74d459082b97bd
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-02 21:25:31 +0000

    Fri, Dec 2, 2022, 9:25 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -126,7 +126,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width ; j++)
         {
-           
+           RGBTRIPLE arr[3][3];
         }
     }
     return;
