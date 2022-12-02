3aacfbc230ea7a57d7a371abfd3585516d2d983f
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-02 04:14:58 +0000

    Fri, Dec 2, 2022, 4:14 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -21,9 +21,9 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
      for(int i = 0; i < height; i++)
     {
-        for(int j = width; j > 0 ; j++)
+        for(int j = 0; j < width; j++)
         {
-            image[i][j].rgbtBlue = avg;
+            image[i][j].rgbtBlue = image[i][].rgbtBlue;
             image[i][j].rgbtGreen = avg;
             image[i][j].rgbtRed = avg;
         }
