e5387fc37b4288198b6f84b19d7292192e5068f9
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-02 21:01:48 +0000

    Fri, Dec 2, 2022, 9:01 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -111,7 +111,18 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-   
+   RGBTRIPLE newImage[height][width];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width ; j++)
+        {
+            newImage[i][j] = image[i][j];
+        }
+    }
+
+
+    
     return;
 }
 
