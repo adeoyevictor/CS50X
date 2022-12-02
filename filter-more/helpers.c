12d840890949445519039d9ea5567c0d2ca99af2
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-02 04:09:45 +0000

    Fri, Dec 2, 2022, 4:09 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -19,6 +19,15 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+     for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtBlue = avg;
+            image[i][j].rgbtGreen = avg;
+            image[i][j].rgbtRed = avg;
+        }
+    }
     return;
 }
 
