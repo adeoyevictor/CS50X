447326854091d1a2f9ee92116e5dc0a3cafb2d3d
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-02 20:37:27 +0000

    Fri, Dec 2, 2022, 8:37 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -151,7 +151,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                         arr[s + 1][t + 1] = 0;
                     }
 
-                    arr[s][t] = image
+                    arr[s][t] = image[s][t];
                 }
             }
 
