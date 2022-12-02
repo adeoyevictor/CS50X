7c05aaefefb0332ee12f77429a053f0516dd3d26
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-02 04:41:50 +0000

    Fri, Dec 2, 2022, 4:41 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -7,7 +7,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for(int j = 0; j < width; j++)
         {
-            int avg  = (int) round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3);
+            double avg  = (int) round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3);
             image[i][j].rgbtBlue = avg;
             image[i][j].rgbtGreen = avg;
             image[i][j].rgbtRed = avg;
