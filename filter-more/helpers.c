aa8399f7b16bfc8aad4a8665adb8b37948d0b58f
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-02 22:11:19 +0000

    Fri, Dec 2, 2022, 10:11 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -133,9 +133,9 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             {
                   gx_blue += (image[i][j + 1].rgbtBlue * 2) + (image[i + 1][j + 1].rgbtBlue * 1)
             }
-            else if (i == 0 && )
+            else if (i == 0 && j == width - 1)
             {
-
+                 gx_blue += (image[i][j].rgbtBlue * 0) + (image[i - 1][j].rgbtBlue * 0) + (image[i + 1][j].rgbtBlue * 0) + (image[i][j - 1].rgbtBlue * -2) + (image[i][j + 1].rgbtBlue * 2) + (image[i - 1][j - 1].rgbtBlue * -1) + (image[i + 1][j - 1].rgbtBlue * -1) + (image[i - 1][j + 1].rgbtBlue * 1) + (image[i + 1][j + 1].rgbtBlue * 1)
             }
 
 
