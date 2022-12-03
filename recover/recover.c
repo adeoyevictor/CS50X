88ee8bca336bd29dbcec478b1ec63d35d3ca8e01
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-03 17:29:45 +0000

    Sat, Dec 3, 2022, 5:29 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
+    while (fread(buffer, 1, 512, input) == 512)
     {
 
 
