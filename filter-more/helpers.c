528c432bbcbf58da84c5abbe0e16f2b5a58bfd26
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-03 10:25:26 +0000

    Sat, Dec 3, 2022, 10:25 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -166,15 +166,15 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                 greeny = (newImage[i - 1][j].rgbtGreen * -2) + (newImage[i - 1][j + 1].rgbtGreen * -1);
                 redy = (newImage[i - 1][j].rgbtRed * -2) + (newImage[i - 1][j + 1].rgbtRed * -1);
             }
-            // else if (i == height - 1 && j == width - 1)
-            // {
-            //     bluex = bluex +  (newImage[i][j - 1].rgbtBlue * -2) +  (newImage[i - 1][j - 1].rgbtBlue * -1);
-            //     greenx = greenx +  (newImage[i][j - 1].rgbtGreen * -2) +  (newImage[i - 1][j - 1].rgbtGreen * -1);
-            //     redx = redx +  (newImage[i][j - 1].rgbtRed * -2) +  (newImage[i - 1][j - 1].rgbtRed * -1);
-            //     bluey = bluey + (newImage[i - 1][j - 1].rgbtBlue * -1) + (newImage[i - 1][j].rgbtBlue * -2);
-            //     greeny = greeny + (newImage[i - 1][j - 1].rgbtGreen * -1) + (newImage[i - 1][j].rgbtGreen * -2);
-            //     redy = redy + (newImage[i - 1][j - 1].rgbtRed * -1) + (newImage[i - 1][j].rgbtRed * -2);
-            // }
+            else if (i == height - 1 && j == width - 1)
+            {
+                bluex = bluex +  (newImage[i][j - 1].rgbtBlue * -2) +  (newImage[i - 1][j - 1].rgbtBlue * -1);
+                greenx = greenx +  (newImage[i][j - 1].rgbtGreen * -2) +  (newImage[i - 1][j - 1].rgbtGreen * -1);
+                redx = redx +  (newImage[i][j - 1].rgbtRed * -2) +  (newImage[i - 1][j - 1].rgbtRed * -1);
+                bluey = bluey + (newImage[i - 1][j - 1].rgbtBlue * -1) + (newImage[i - 1][j].rgbtBlue * -2);
+                greeny = greeny + (newImage[i - 1][j - 1].rgbtGreen * -1) + (newImage[i - 1][j].rgbtGreen * -2);
+                redy = redy + (newImage[i - 1][j - 1].rgbtRed * -1) + (newImage[i - 1][j].rgbtRed * -2);
+            }
             // else if (i == 0)
             // {
             //       bluex = bluex + (newImage[i][j - 1].rgbtBlue * -2) + (newImage[i][j + 1].rgbtBlue * 2) +  (newImage[i + 1][j - 1].rgbtBlue * -1)  + (newImage[i + 1][j + 1].rgbtBlue * 1);
