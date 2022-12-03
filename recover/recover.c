6b78a334a70ba2341d65475bd474d8fdd2480be6
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-03 18:15:24 +0000

    Sat, Dec 3, 2022, 6:15 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -28,6 +28,9 @@ int main(int argc, char *argv[])
         if (read[0] == 0xff && read[1] == 0xd8 && read[2] == 0xff && (read[3] & 0xf0) == 0xe0)
         {
             if (count == 0)
+            {
+                
+            }
 
             // char *name = malloc(8);
             // sprintf(name, "%03i.jpg", count);
