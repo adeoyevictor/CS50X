7aebbefe608d8ffc9ca84f4f37a78ed071d06bd9
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-03 11:42:31 +0000

    Sat, Dec 3, 2022, 11:42 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -218,7 +218,9 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 
                 bluey = bluey + (newImage[i - 1][j - 1].rgbtBlue * -1) + (newImage[i - 1][j].rgbtBlue * -2) + (newImage[i - 1][j + 1].rgbtBlue * -1) + (newImage[i + 1][j - 1].rgbtBlue * 1) + (newImage[i + 1][j ].rgbtBlue * 2) + (newImage[i + 1][j + 1].rgbtBlue * 1);
 
-                greeny = greeny + (newImage[i - 1][j - 1].rgbtGreen * -1) + (newImage[i - 1][j].rgbtGreen * -2) + (newImage[i - 1][j + 1].rgbtGreen * -1) + (newImage[i + 1][j - 1].rgbtGreen * 1) + (newImage[i + 1][j ].rgbtGreen * 2) + (newImage[i + 1][j + 1].rgbtGreen * 1);
+                greeny = greeny + (newImage[i - 1][j - 1].rgbtGreen * -1) + (newImage[i - 1][j].rgbtGreen * -2) +
+                         (newImage[i - 1][j + 1].rgbtGreen * -1) + (newImage[i + 1][j - 1].rgbtGreen * 1) + (newImage[i + 1][j ].rgbtGreen * 2) +
+                         (newImage[i + 1][j + 1].rgbtGreen * 1);
 
                 redy = redy + (newImage[i - 1][j - 1].rgbtRed * -1) + (newImage[i - 1][j].rgbtRed * -2) + (newImage[i - 1][j + 1].rgbtRed * -1) +
                        (newImage[i + 1][j - 1].rgbtRed * 1) + (newImage[i + 1][j ].rgbtRed * 2) + (newImage[i + 1][j + 1].rgbtRed * 1);
