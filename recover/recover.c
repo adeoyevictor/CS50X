4424da4173cd31a066fbbb5d945ba6b0497ff878
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-03 18:12:03 +0000

    Sat, Dec 3, 2022, 6:12 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -21,12 +21,14 @@ int main(int argc, char *argv[])
 
     BYTE read[512];
 
-    // int count = 0;
+    int count = 0;
 
     while (fread(read, 1, 512, input) == 512)
     {
         if (read[0] == 0xff && read[1] == 0xd8 && read[2] == 0xff && (read[3] & 0xf0) == 0xe0)
         {
+            if (count == 0)
+
             // char *name = malloc(8);
             // sprintf(name, "%03i.jpg", count);
             // FILE *img = fopen(name, "w");
