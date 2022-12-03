a81b624d6bac066c513cbe0f670d36b3f0c586b3
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-03 16:27:01 +0000

    Sat, Dec 3, 2022, 4:27 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -9,11 +9,11 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    FILE *f = fopen(argv[1], 'r');
-    if (f == NULL)
-    {
-        printf("Could not open file.\n");
-        return 1;
-    }
+    FILE *input = fopen(argv[1], 'r');
+    // if (f == NULL)
+    // {
+    //     printf("Could not open file.\n");
+    //     return 1;
+    // }
 
 }
