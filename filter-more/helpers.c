bd647cfbe8d1bf6a6c6ebd2e2c214ec23ec9385e
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-03 11:05:32 +0000

    Sat, Dec 3, 2022, 11:05 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -112,73 +112,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE newImage[height][width];
-
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width ; j++)
-        {
-            newImage[i][j] = image[i][j];
-        }
-    }
-
-    int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
-    int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
-
-      for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width ; j++)
-        {
-
-            float redx = 0;
-            float greenx = 0;
-            float bluex = 0;
-            float redy = 0;
-            float greeny = 0;
-            float bluey = 0;
-
-            for (int s = -1; s < 2; s++)
-            {
-                for (int t = -1; t < 2; t++)
-                {
-                    if (i + s < 0 || i + s >= height)
-                    {
-                        continue;
-                    }
-                    if (j + t < 0 || j + t >= width)
-                    {
-                        continue;
-                    }
-                    redx += newImage[i + s][j + t].rgbtRed * Gx[s + 1][t + 1];
-                    greenx += newImage[i + s][j + t].rgbtGreen * Gx[s + 1][t + 1];
-                    bluex += newImage[i + s][j + t].rgbtBlue * Gx[s + 1][t + 1];
-                    redy += newImage[i + s][j + t].rgbtRed * Gy[s + 1][t + 1];
-                    greeny += newImage[i + s][j + t].rgbtGreen * Gy[s + 1][t + 1];
-                    bluey += newImage[i + s][j + t].rgbtBlue * Gy[s + 1][t + 1];
-
-                    int blue = round(sqrt((bluex * bluex) + (bluey * bluey)));
-                    int green = round(sqrt((greenx * greenx) + (greeny * greeny)));
-                    int red =  round(sqrt((redx * redx) + (redy * redy)));
-
-                    if (red > 255)
-                    {
-                        red = 255;
-                    }
-                    if (green > 255)
-                    {
-                        green = 255;
-                    }
-                    if (blue > 255)
-                    {
-                        blue = 255;
-                    }
-                    image[i][j].rgbtBlue = blue;
-                    image[i][j].rgbtGreen = green;
-                    image[i][j].rgbtRed = red;
-                }
-            }
-        }
-    }
+   
     return;
 }
 
