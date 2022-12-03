4443fc1cac4ccb713e1467cdb841e07465fab1be
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-03 01:10:40 +0000

    Sat, Dec 3, 2022, 1:10 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -160,7 +160,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                 redx = redx +  (newImage[i][j + 1].rgbtRed * 2)  + (newImage[i - 1][j + 1].rgbtRed * 1);
                 bluey = bluey +  (newImage[i - 1][j].rgbtBlue * -2) + (newImage[i - 1][j + 1].rgbtBlue * -1);
                 greeny = greeny +  (newImage[i - 1][j].rgbtGreen * -2) + (newImage[i - 1][j + 1].rgbtGreen * -1);
-                redy = redy +  (newImage[i - 1][j].rgbtBlue * -2) + (newImage[i - 1][j + 1].rgbtBlue * -1);
+                redy = redy +  (newImage[i - 1][j].rgbtRed * -2) + (newImage[i - 1][j + 1].rgbtRed * -1);
             }
             else if (i == height - 1 && j == width - 1)
             {
