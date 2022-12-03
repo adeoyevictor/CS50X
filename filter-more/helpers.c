aee69710664805d8268ad602768c739e6943e3d5
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-03 00:45:06 +0000

    Sat, Dec 3, 2022, 12:45 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -193,6 +193,24 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             }
 
 
+
+
+
+
+
+
+
+            else
+            {
+                bluex = bluex + (newImage[i][j - 1].rgbtBlue * -2) + (newImage[i][j + 1].rgbtBlue * 2) + (newImage[i - 1][j - 1].rgbtBlue * -1) + (newImage[i + 1][j - 1].rgbtBlue * -1) + (newImage[i - 1][j + 1].rgbtBlue * 1) + (newImage[i + 1][j + 1].rgbtBlue * 1);
+
+                greenx = greenx + (newImage[i][j - 1].rgbtGreen * -2) + (newImage[i][j + 1].rgbtGreen * 2) + (newImage[i - 1][j - 1].rgbtGreen * -1) + (newImage[i + 1][j - 1].rgbtGreen * -1) + (newImage[i - 1][j + 1].rgbtGreen * 1) + (newImage[i + 1][j + 1].rgbtGreen * 1);
+
+                redx = redx + (newImage[i][j - 1].rgbtRed * -2) + (newImage[i][j + 1].rgbtRed * 2) + (newImage[i - 1][j - 1].rgbtRed * -1) + (newImage[i + 1][j - 1].rgbtRed * -1) + (newImage[i - 1][j + 1].rgbtRed * 1) + (newImage[i + 1][j + 1].rgbtRed * 1);
+
+            }
+
+
             // image[i][j].rgbtBlue = (int)sqrt(bluex * bluex);
             // image[i][j].rgbtGreen = (int)sqrt(greenx * bluex);
             // image[i][j].rgbtRed = (int)sqrt(redx * bluex);
