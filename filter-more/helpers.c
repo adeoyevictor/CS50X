e21b000ff9b9f32d4c2c5951c57657e3a08feeaa
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-03 01:03:52 +0000

    Sat, Dec 3, 2022, 1:03 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -137,14 +137,20 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             if (i == 0 && j == 0)
             {
                   bluex = bluex + (newImage[i][j + 1].rgbtBlue * 2) + (newImage[i + 1][j + 1].rgbtBlue * 1);
-                  greenx = greenx + (image[i][j + 1].rgbtGreen * 2) + (newImage[i + 1][j + 1].rgbtGreen * 1);
+                  greenx = greenx + (newImage[i][j + 1].rgbtGreen * 2) + (newImage[i + 1][j + 1].rgbtGreen * 1);
                   redx = redx + (newImage[i][j + 1].rgbtRed * 2) + (newImage[i + 1][j + 1].rgbtRed * 1);
+                  bluey = bluey +  (newImage[i + 1][j].rgbtBlue * 2) + (newImage[i + 1][j + 1].rgbtBlue * 1);
+                  greeny = greeny +  (newImage[i + 1][j].rgbtGreen * 2) + (newImage[i + 1][j + 1].rgbtGreen * 1);
+                  redy = redy +  (newImage[i + 1][j].rgbtRed * 2) + (newImage[i + 1][j + 1].rgbtRed * 1);
+
             }
             else if (i == 0 && j == width - 1)
             {
                  bluex = bluex + (newImage[i][j - 1].rgbtBlue * -2)  +  (newImage[i + 1][j - 1].rgbtBlue * - 1);
                  greenx = greenx + (newImage[i][j - 1].rgbtGreen * -2)  +  (newImage[i + 1][j - 1].rgbtGreen * - 1);
                  redx = redx + (newImage[i][j - 1].rgbtRed * -2)  +  (newImage[i + 1][j - 1].rgbtRed * - 1);
+
+                 bluey = bluey + (newImage[i + 1][j - 1].rgbtBlue * 1) + (newImage[i + 1][j ].rgbtBlue * 2) + (newImage[i + 1][j + 1].rgbtBlue * 1);
             }
             else if (i == height - 1 && j == 0)
             {
@@ -190,22 +196,13 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 
                 redx = redx + (newImage[i][j - 1].rgbtRed * -2) + (newImage[i][j + 1].rgbtRed * 2) + (newImage[i - 1][j - 1].rgbtRed * -1) + (newImage[i + 1][j - 1].rgbtRed * -1) + (newImage[i - 1][j + 1].rgbtRed * 1) + (newImage[i + 1][j + 1].rgbtRed * 1);
 
-            }
+                bluey = bluey + (newImage[i - 1][j - 1].rgbtBlue * -1) + (newImage[i - 1][j].rgbtBlue * -2) + (newImage[i - 1][j + 1].rgbtBlue * -1) + (newImage[i + 1][j - 1].rgbtBlue * 1) + (newImage[i + 1][j ].rgbtBlue * 2) + (newImage[i + 1][j + 1].rgbtBlue * 1);
 
+                greeny = greeny + (newImage[i - 1][j - 1].rgbtGreen * -1) + (newImage[i - 1][j].rgbtGreen * -2) + (newImage[i - 1][j + 1].rgbtGreen * -1) + (newImage[i + 1][j - 1].rgbtGreen * 1) + (newImage[i + 1][j ].rgbtGreen * 2) + (newImage[i + 1][j + 1].rgbtGreen * 1);
 
-
-
-
-
-
-
-
-            else
-            {
-               
+                redy = redy + (newImage[i - 1][j - 1].rgbtRed * -1) + (newImage[i - 1][j].rgbtRed * -2) + (newImage[i - 1][j + 1].rgbtRed * -1) + (newImage[i + 1][j - 1].rgbtRed * 1) + (newImage[i + 1][j ].rgbtRed * 2) + (newImage[i + 1][j + 1].rgbtRed * 1);
 
             }
-
 
             // image[i][j].rgbtBlue = (int)sqrt(bluex * bluex);
             // image[i][j].rgbtGreen = (int)sqrt(greenx * bluex);
