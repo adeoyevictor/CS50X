130bc0483eee97159b697cc59e724ab2a2d18e7f
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-10 00:32:19 +0000

    Sat, Dec 10, 2022, 12:32 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -32,8 +32,9 @@ bool check(const char *word)
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    
+
     // TODO: Improve this hash function
+    
     return toupper(word[0]) - 'A';
 }
 
