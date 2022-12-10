aa20e41d562760ad4c32f14c2f6ce74f6c853500
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-10 00:33:07 +0000

    Sat, Dec 10, 2022, 12:33 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -17,7 +17,7 @@ typedef struct node
 node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
+const unsigned int N = ;
 
 // Hash table
 node *table[N];
@@ -34,7 +34,7 @@ unsigned int hash(const char *word)
 {
 
     // TODO: Improve this hash function
-    
+
     return toupper(word[0]) - 'A';
 }
 
