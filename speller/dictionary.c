cb7bb8e05941ba2ecde9a1f22ca9f1fe510a3ad6
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-10 09:10:26 +0000

    Sat, Dec 10, 2022, 9:10 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -108,7 +108,7 @@ bool unload(void)
         node *cursor = table[i];
         node *temp = cursor;
 
-        while (cursor != NULL)
+        while (cursor !`= NULL)
         {
             cursor = cursor -> next;
             free(temp);
