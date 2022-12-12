9ee02eb16d49b19bbbe2115dac4542032ca7f165
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-12 23:41:08 +0000

    Mon, Dec 12, 2022, 11:41 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,1 +1,3 @@
-SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Chadwick Boseman");
+SELECT title FROM movies WHERE id IN (
+SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Chadwick Boseman")
+);
