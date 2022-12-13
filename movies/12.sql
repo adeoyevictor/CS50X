3d1018eaca40a6d5e990ad24021b3b29df8ac7bf
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-13 00:01:50 +0000

    Tue, Dec 13, 2022, 12:01 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,6 +1,9 @@
--- SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE person_id IN (
+SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id WHERE 
+
+
+-- JOIN people ON stars.person_id = people.id WHERE person_id IN (
 -- );
 
-SELECT id FROM people WHERE name = "Johnny Depp"; --OR name = "Helena Bonham Carter"
+-- SELECT id FROM people WHERE name = "Johnny Depp"; --OR name = "Helena Bonham Carter"
 
-SELECT title FROM movies WHERE 
+-- SELECT title FROM movies WHERE
