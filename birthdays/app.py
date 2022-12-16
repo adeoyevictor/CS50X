d5a7b1210f275f1d5791ceaabda4ced38c765952
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-16 20:48:45 +0000

    Fri, Dec 16, 2022, 8:48 PM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -36,7 +36,6 @@ def index():
 
     else:
 
-        db.execute("DELETE FROM birthdays WHERE id IN (SELECT id FROM birthdays WHERE name = 'Opa')")
         # TODO: Display the entries in the database on index.html
         birthdays = db.execute("SELECT * FROM birthdays")
         return render_template("index.html", birthdays=birthdays)
