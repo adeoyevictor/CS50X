395c46f3ad0ebc821a74db43c5de8c119fc99365
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-19 23:00:50 +0000

    Mon, Dec 19, 2022, 11:00 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -133,7 +133,7 @@ def register():
         elif password != confirmation:
             return apology("password and confirmation must be the same")
         else:
-            
+            db.execute("")
 
         return apology("TODO")
 
