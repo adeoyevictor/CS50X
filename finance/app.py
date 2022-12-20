3f415a8afe63425365ab2ea1b77a84b6ef16ea24
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-20 21:21:04 +0000

    Tue, Dec 20, 2022, 9:21 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -43,7 +43,7 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    
+    stocks = db.execute("SELECT * FROM stocks WHERE user_id=?", session["user_id"])
     return apology("TODO")
 
 
