f38afef8118c39fea0a4a4863cfec2ac16ce23fb
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-20 22:13:52 +0000

    Tue, Dec 20, 2022, 10:13 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -182,4 +182,7 @@ def register():
 @login_required
 def sell():
     """Sell shares of stock"""
-    return apology("TODO")
+    if request.method == "GET":
+        return render_template("sell.html")
+    else:
+        
