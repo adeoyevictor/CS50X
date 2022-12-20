b2c6805b3d10345ee4d51c51b33e183eb7975c2d
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-20 21:22:10 +0000

    Tue, Dec 20, 2022, 9:22 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -44,7 +44,7 @@ def after_request(response):
 def index():
     """Show portfolio of stocks"""
     stocks = db.execute("SELECT * FROM stocks WHERE user_id=?", session["user_id"])
-    return apology("TODO")
+    return render_template("index.html", stocks=stocks)
 
 
 @app.route("/buy", methods=["GET", "POST"])
