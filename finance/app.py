df49aae574ea702c536f74b8b69e79026575cd84
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-21 12:38:23 +0000

    Wed, Dec 21, 2022, 12:38 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -72,8 +72,8 @@ def buy():
         result = lookup(symbol)
         if not result:
              return apology("Invalid Symbol", 400)
-        shares = request.form.get("shares")
-        if not shares or int(shares) <= 0:
+        shares = int(request.form.get("shares"))
+        if not shares or int(shares) < 1:
             return apology("Invalid shares", 400)
         cash = db.execute("SELECT cash FROM users WHERE id =?", session["user_id"])
         amount = int(shares) * float(result["price"])
