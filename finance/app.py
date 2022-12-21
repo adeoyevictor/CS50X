1d097168ce051892c7bfa4a3f9c1367636fab3c2
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2022-12-21 10:03:51 +0000

    Wed, Dec 21, 2022, 10:03 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -211,7 +211,10 @@ def sell():
 
         db.execute("UPDATE users SET cash =? WHERE id=?", cash[0]["cash"] + selling_price, session["user_id"])
 
-        db.execute("UPDATE stocks SET shares =? WHERE user_id=? AND stock=?", cash[0]["cash"] + selling_price, session["user_id"])
+        shares = db.execute("SELECT shares FROM stocks WHERE user_id =? AND stock=?", session["user_id"], symbol)
+
+
+        db.execute("UPDATE stocks SET shares =? WHERE user_id=? AND stock=?", shares[0]["cash"] + selling_price, session["user_id"])
 
 
 
