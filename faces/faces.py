90ae9943578f4b7933d77cf1a097c61934818af4
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-02 00:33:28 +0000

    Mon, Jan 2, 2023, 12:33 AM +00:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,11 +1,11 @@
 def convert(text):
-    result = text.replace(":)", "🙂")
-    result = text.replace(":(", "🙁")
+    result = text.replace(":)", "🙂").replace(":(", "🙁")
     return result
 
 
 def main():
     text = input("Enter a text: ")
+
     print(convert(text))
 
 main()
