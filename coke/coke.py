64673b7d517258086ea1d391cb40ffa6d7b90bfe
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-03 09:41:13 +0000

    Tue, Jan 3, 2023, 9:41 AM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -2,11 +2,8 @@ amount = 0
 
 while True:
     print("Amount Due:", 50 - amount)
-    try:
-        x = int(input("Insert Coin: "))
-        amount += x
-    except:
-        pass
+    x = int(input("Insert Coin: "))
+    amount += x
     if amount >= 50:
         print("Change Owed:", amount - 50)
         break
