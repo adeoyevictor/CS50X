7d2d45dac961d2152f02caa78c27a6743835ffb8
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-04 11:17:10 +0000

    Wed, Jan 4, 2023, 11:17 AM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -10,14 +10,12 @@ def is_valid(s):
     if s[:2].isalpha() and s.isalnum() and len(s) >= 2 and len(s) <=6:
         for i in range(len(s)):
             if s[i].isnumeric():
-                idx = i
+                print("numeric")
+                print(s[i])
                 break
             else:
-                return True
-        if s[idx] == 0:
-            return False
-        if s[idx:].isnumeric():
-            return True
+                print("not numeric")
+                print(s[i])
     return False
 
 
