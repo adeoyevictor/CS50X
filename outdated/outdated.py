45ab68300068cc72a03c215f9026ac44b71d4c24
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-05 12:43:42 +0000

    Thu, Jan 5, 2023, 12:43 PM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -13,20 +13,22 @@ months = [
     "December"
 ]
 
-dt = input("Date: ")
+def main():
+    dt = input("Date: ")
 
-if '/' in dt:
-    month, day, year = dt.split('/')
-    frmt(year, month, day)
-elif ',' in dt:
-    month, day, year = dt.split(' ')
-    day = day[:-1]
-    for i in range(len(months)):
-        if months[i] == month.capitalize():
-            month = i + 1
-    frmt(year, month, day)
+    if '/' in dt:
+        month, day, year = dt.split('/')
+        frmt(year, month, day)
+    elif ',' in dt:
+        month, day, year = dt.split(' ')
+        day = day[:-1]
+        for i in range(len(months)):
+            if months[i] == month.capitalize():
+                month = i + 1
+        frmt(year, month, day)
 
 def frmt(year, month, day):
     print(year, month, day, sep="-")
 
 
+main()
