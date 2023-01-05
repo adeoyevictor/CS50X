7c9ceb5de08b181f47bd3ac410f0e47f2fb71fd7
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-05 12:41:55 +0000

    Thu, Jan 5, 2023, 12:41 PM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -17,8 +17,16 @@ dt = input("Date: ")
 
 if '/' in dt:
     month, day, year = dt.split('/')
+    format(year, month, day)
 elif ',' in dt:
     month, day, year = dt.split(' ')
     day = day[:-1]
-    
+    for i in range(len(months)):
+        if months[i] == month.capitalize():
+            month = i + 1
+    format(year, month, day)
+
+def format(year, month, day):
+    print(year, month, day, sep="-")
+
 
