a4db711b74b0202edef8d841eba33d56bc111e5a
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-06 20:51:15 +0000

    Fri, Jan 6, 2023, 8:51 PM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -6,10 +6,13 @@ figlet = Figlet()
 lst = figlet.getFonts()
 
 if len(sys.argv) == 1:
-    pass
+    f = random.choice(lst)
+    figlet.setFont(font=f)
 elif len(sys.argv) == 3 and (sys.argv[1] == '-f' or sys.argv[1] == '--font'):
-    pass
+    figlet.setFont(font=sys.argv[2])
 else:
     sys.exit("Invalid arguments")
 
 txt = input("Input: ")
+
+print(figlet.renderText(txt))
