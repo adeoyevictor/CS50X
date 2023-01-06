1ed2d2cadcc4949ae33abc257592de80e5e63013
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-06 20:45:58 +0000

    Fri, Jan 6, 2023, 8:45 PM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -3,9 +3,9 @@ import sys
 txt = input("Input: ")
 
 if len(sys.argv) == 1:
-
+    pass
 elif len(sys.argv) == 3 and (sys.argv[1] == '-f' or sys.argv[1] == '--font'):
-
+    pass
 else:
     sys.exit("Invalid arguments")
 
