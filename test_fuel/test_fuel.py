ce91aa4be8d04ae024247c13bc616051cde2f9f5
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-07 20:33:49 +0000

    Sat, Jan 7, 2023, 8:33 PM +00:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,7 +1,15 @@
 from fuel import convert, gauge
-
+import pytest
 
 def test_convert():
-    
+    with pytest.raises(ValueError):
+        convert("4.5")
+    with pytest.raises(ValueError):
+        convert("cat/dog")
+    # with pytest.raises(ValueError):
+    #     convert("6/3")
+    with pytest.raises(ZeroDivisionError):
+        convert("6/0")
 
 def test_gauge():
+    pass
