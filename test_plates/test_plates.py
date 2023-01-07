3df7bda13cf1f4e5de0ce6d1f0ae1ef83132e80b
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-07 19:29:45 +0000

    Sat, Jan 7, 2023, 7:29 PM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,13 +1,14 @@
 from plates import is_valid
 
+def test_2():
+    assert is_valid("cs") == True
+    assert is_valid("45") == False
 
 def test_1():
     assert is_valid("CS50WERGH") == False
     assert is_valid("C") == False
     assert is_valid("CS50") == True
-def test_2():
-    assert is_valid("cs") == True
-    assert is_valid("45") == False
+
 def test_3():
     assert is_valid("cs50") == True
     assert is_valid("cs50%@") == False
