80d9caec88ce8b132d2505382706e4f12725a9fb
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-07 14:12:40 +0000

    Sat, Jan 7, 2023, 2:12 PM +00:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -2,6 +2,9 @@ from twttr import shorten
 
 def test_hello():
     assert shorten("Hello") == "Hll"
+    assert shorten("hello") == "hll"
+    assert shorten("hello1") == "hll1"
 
 def test_World():
     assert shorten("World") == "Wrld"
+    assert shorten("World,") == "Wrld,"
