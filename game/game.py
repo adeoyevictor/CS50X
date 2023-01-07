4f69099d603148af0d91335c385683929fc07202
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-07 01:06:03 +0000

    Sat, Jan 7, 2023, 1:06 AM +00:00

--- game/game.py
+++ game/game.py
@@ -12,4 +12,21 @@ while True:
             break
 
 num = random.randint(1, level)
-print(num)
+
+while True:
+    try:
+        guess = int(input("Guess: "))
+    except ValueError:
+        pass
+    else:
+        if guess < : 1
+            continue
+        else:
+            if guess < num:
+                print("Too small!")
+                continue
+            elif guess > num:
+                print("Too large!")
+                continue
+            else:
+                print("Just right!")
