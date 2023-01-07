d97024a5359e10f37df16a63dd7eb2ab334714be
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-07 19:36:02 +0000

    Sat, Jan 7, 2023, 7:36 PM +00:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -1,3 +1,18 @@
+def main():
+    ...
+
+
+def convert(fraction):
+    ...
+
+
+def gauge(percentage):
+    ...
+
+
+if __name__ == "__main__":
+    main()
+
 while True:
     try:
         x , y = input("Fraction: ").split("/")
