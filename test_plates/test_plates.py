1926ce4e0d0461e14b2e8b4d36db95a384de8fa0
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-07 19:00:36 +0000

    Sat, Jan 7, 2023, 7:00 PM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -3,9 +3,9 @@ from plates import is_valid
 
 def test_1():
     assert is_valid("CS50") == True
-    assert is_valid("cs508") == True
 def test_2():
-
+    assert is_valid("cs508") == True
 def test_3():
-
+    assert is_valid("HPACB") == True
 def test_4():
+    assert is_valid("EM") == True
