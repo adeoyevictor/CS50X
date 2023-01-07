e005b35c8e4323459a69a7c59b04683c81b12bf6
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-07 14:19:54 +0000

    Sat, Jan 7, 2023, 2:19 PM +00:00

---
+++ test_bank/bank.py
@@ -0,0 +1,26 @@
+def main():
+    ...
+
+
+def value(greeting):
+    if greeting.startswith("hello"):
+        print("$0")
+    elif greeting.startswith("h"):
+        print("$20")
+    else:
+        print("$100")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+text = input("Greeting: ").strip().lower()
+
+
+
+
+
