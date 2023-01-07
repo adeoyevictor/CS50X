423cf728592866f26f3911193dcf6314c4d486b8
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-07 18:52:59 +0000

    Sat, Jan 7, 2023, 6:52 PM +00:00

---
+++ test_plates/plates.py
@@ -0,0 +1,25 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if s[:2].isalpha() and s.isalnum() and len(s) >= 2 and len(s) <=6:
+        if s[2:].isalpha():
+            return True
+        for i in range(len(s)):
+            if s[i].isnumeric():
+                if s[i] != '0' and s[i:].isnumeric():
+                    return True
+                break
+
+    return False
+
+
+if __name__ == "__main__":
+    main()
+
+def main():
