9f87900ae2ea3bc3deb76baa1b158aa7bef439f0
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-07 19:35:23 +0000

    Sat, Jan 7, 2023, 7:35 PM +00:00

---
+++ test_fuel/fuel.py
@@ -0,0 +1,17 @@
+while True:
+    try:
+        x , y = input("Fraction: ").split("/")
+        x = int(x)
+        y = int(y)
+        if x > y:
+            continue
+        percent =  round((x / y) * 100)
+        if percent <= 1:
+            print("E")
+        elif percent >= 99:
+            print("F")
+        else:
+            print(f"{percent}%")
+        break
+    except (ValueError,  ZeroDivisionError):
+        pass
