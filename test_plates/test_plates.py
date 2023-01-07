f7a7a7b19d39302faba1aff6ff15fed0969d7ffa
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-07 19:03:14 +0000

    Sat, Jan 7, 2023, 7:03 PM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -7,5 +7,8 @@ def test_2():
     assert is_valid("cs508") == True
 def test_3():
     assert is_valid("HPACB") == True
+    assert is_valid("HPA09") == False
 def test_4():
     assert is_valid("EM") == True
+    assert is_valid("89Mg") == False
+    assert is_valid("89Mgtyhg") == False
