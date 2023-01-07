b38c061b2d462526e4e2f1b4cab32b155b1d099a
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-07 14:31:21 +0000

    Sat, Jan 7, 2023, 2:31 PM +00:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -8,11 +8,11 @@ def main():
 def value(greeting):
     greeting = greeting.lower()
     if greeting.startswith("hello"):
-        return f"0"
+        return f"$0"
     elif greeting.startswith("h"):
-        return f"20"
+        return f"$20"
     else:
-        return f"100"
+        return f"$100"
 
 
 if __name__ == "__main__":
--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,13 +1,13 @@
 from bank import value
 
 def test_1():
-    assert value("hello") == "0"
-    assert value("Hello") == "0"
+    assert value("hello") == "$0"
+    assert value("Hello") == "$0"
 
 def test_2():
-    assert value("hi") == "20"
-    assert value("Hey") == "20"
+    assert value("hi") == "$20"
+    assert value("Hey") == "$20"
 
 def test_2():
-    assert value("good morning") == "100"
-    assert value("good evening") == "100"
+    assert value("good morning") == "$100"
+    assert value("good evening") == "$100"
