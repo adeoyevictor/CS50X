5af2adfcb0ad221c7aa4f2567100807722f2d478
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-07 14:39:37 +0000

    Sat, Jan 7, 2023, 2:39 PM +00:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -1,18 +1,18 @@
 def main():
     text = input("Greeting: ").strip()
     res = value(text)
-    print(res)
+    print(f"${res}")
 
 
 
 def value(greeting):
     greeting = greeting.lower()
     if greeting.startswith("hello"):
-        return f"$0"
+        return 0
     elif greeting.startswith("h"):
-        return f"$20"
+        return 20
     else:
-        return f"$100"
+        return 100
 
 
 if __name__ == "__main__":
