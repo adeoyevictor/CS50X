aca3a23ab8d4b1a2b655b45a94812fb4fde4b729
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-09 09:48:52 +0000

    Mon, Jan 9, 2023, 9:48 AM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,12 +1,19 @@
 import sys
 
+lines = 0
+
 if len(sys.argv) == 2 and sys.argv[1].strip().endswith('.py'):
     try:
         with open(sys.argv[1]) as file:
             for line in file:
-                if line.lstrip().startswith("#")
+                if line.lstrip().startswith("#"):
+                    pass
+                else:
+                    lines += 1
     except FileNotFoundError:
         sys.exit("An Error Occured")
 
 else:
     sys.exit("Invalid File")
+
+print(lines)
