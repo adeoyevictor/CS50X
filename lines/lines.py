be5eb72939893d3576ec63924d4c2ca50875909d
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-09 09:44:49 +0000

    Mon, Jan 9, 2023, 9:44 AM +00:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,3 +1,11 @@
 import sys
 
 if len(sys.argv) == 2 and sys.argv[1].strip().endswith('.py'):
+    try:
+        with open(sys.argv[1]) as file:
+
+    except FileNotFoundError:
+        sys.exit("An Error Occured")
+
+else:
+    sys.exit("An Error Occured")
