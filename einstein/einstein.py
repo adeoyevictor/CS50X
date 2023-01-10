86b7e9e614e1a58e5b5f9df7cc48ae6fa9ee8fdf
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-10 10:43:18 +0000

    Tue, Jan 10, 2023, 10:43 AM +00:00

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -1,8 +1,7 @@
 try:
     mass = int(input("m: "))
+    E = mass * (300000000 ** 2)
+    print(f"E: {E}")
 except ValueError:
     print("that's not an int")
     exit()
-else:
-    E = mass * (300000000 ** 2)
-    print(f"E: {E}")
