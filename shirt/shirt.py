3ec183abe9d05819ef34cbfc0e86a5f76cd47c80
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-11 22:22:17 +0000

    Wed, Jan 11, 2023, 10:22 PM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,19 +1,23 @@
 import sys
 import os
-import PIL
+from PIL import Image, ImageOps
 
 exts = ['.jpg', '.jpeg', '.png']
-
 
 if len(sys.argv) == 3:
     ext1 = os.path.splitext(sys.argv[1])[1].lower()
     ext2 = os.path.splitext(sys.argv[2])[1].lower()
     if ext1 in exts and ext2 in exts and ext1 == ext2:
         try:
-            shirt = PIL.Image.open(sys.argv[1])
+            overlay = Image.open("shirt.png")
+            size = overlay.size
+            shirt = Image.open(sys.argv[1])
+            shirt = ImageOps.fit(shirt, size, method=, bleed=0.0, centering=(0.5, 0.5))
+            shirt.paste(overlay)
+            shirt.save(sys.argv[2])
         except FileNotFoundError:
             sys.exit()
     else:
-        sys.exit()
+        sys.exit("")
 else:
     sys.exit()
