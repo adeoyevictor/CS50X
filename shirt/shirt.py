b41e62ba3b735cbd32e900b80472921cfb2d1f26
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-11 10:32:19 +0000

    Wed, Jan 11, 2023, 10:32 AM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+import PIL
 
 exts = ['.jpg', '.jpeg', '.png']
 
@@ -9,7 +9,10 @@ if len(sys.argv) == 3:
     ext1 = os.path.splitext(sys.argv[1])[1].lower()
     ext2 = os.path.splitext(sys.argv[2])[1].lower()
     if ext1 in exts and ext2 in exts and ext1 == ext2:
-
+        try:
+            shirt = PIL.Image.open(sys.argv[1])
+        except FileNotFoundError:
+            sys.exit()
     else:
         sys.exit()
 else:
