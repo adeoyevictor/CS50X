09f94d825f10a6cf0cd91b3390aab206ca58a2e9
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-11 10:20:21 +0000

    Wed, Jan 11, 2023, 10:20 AM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -3,6 +3,8 @@ import os
 
 if len(sys.argv) == 3:
     ext1 = os.path.splitext(sys.argv[1])[1]
-    print(ext1)
+    ext2 = os.path.splitext(sys.argv[2])[1]
+    
+    print(ext1, ext2)
 else:
     sys.exit()
