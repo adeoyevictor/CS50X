e41d61b3f896b69def9cdc6d3328219a37c5f7b5
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-14 20:29:27 +0000

    Sat, Jan 14, 2023, 8:29 PM +00:00

--- working/working.py
+++ working/working.py
@@ -7,8 +7,9 @@ def main():
 
 
 def convert(s):
-    if re.search(r"^[0-9]{1,2}:?([0-9]{2})? (AM|PM) to [0-9]{1,2}:?([0-9]{2})? (AM|PM)$", s):
-        print("Yes")
+    if matches := re.search(r"^([0-9]{1,2}:?([0-9]{2})? (AM|PM)) to ([0-9]{1,2}:?([0-9]{2})? (AM|PM))$", s):
+        print(matches.group(1))
+        print(matches.group(2))
 
 
 if __name__ == "__main__":
