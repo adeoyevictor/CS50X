60489f6188c22a10bb832d2e1ed6ab911fb9b51a
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-14 21:18:56 +0000

    Sat, Jan 14, 2023, 9:18 PM +00:00

--- working/working.py
+++ working/working.py
@@ -40,11 +40,8 @@ def convert_valid_time(t):
     if am_or_pm == 'AM':
         if int(hr) == 12:
             hr = 0
-        
-
-        ...
     elif am_or_pm == 'PM':
-        ...
+        if int(hr) != 12:
 
 
 if __name__ == "__main__":
