76ffb8e1c9fcc40f97173966bc9396fcdba99f55
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-14 21:02:37 +0000

    Sat, Jan 14, 2023, 9:02 PM +00:00

--- working/working.py
+++ working/working.py
@@ -10,9 +10,8 @@ def convert(s):
     if matches := re.search(r"^([0-9]{1,2}:?(?:[0-9]{2})? (?:AM|PM)) to ([0-9]{1,2}:?(?:[0-9]{2})? (?:AM|PM))$", s):
         start, stop = matches.groups()
         if check_valid_time(start) and check_valid_time(stop):
-            print("Valid Time")
-            print(start, stop)
-            
+
+
         else:
             raise ValueError("Invalid Time")
 
@@ -20,7 +19,18 @@ def convert(s):
         raise ValueError("Invalid Time")
 
 def check_valid_time(t):
-    # am_or_pm = t[-2:]
+    time = t.split(" ")[0]
+    if ":" in time:
+        hr, min = time.split(":")
+        if int(hr) > 12 or int(hr) < 1 or int(min) > 59 or int(min) < 0:
+            return False
+    else:
+        if int(time) > 12 or int(time) < 1:
+            return False
+    return True
+
+def convert_valid_time(t):
+    am_or_pm = t[-2:]
     time = t.split(" ")[0]
     if ":" in time:
         hr, min = time.split(":")
