785417c85bfc2752f2668a235d03e87c63c88ef2
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-14 17:57:18 +0000

    Sat, Jan 14, 2023, 5:57 PM +00:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -7,19 +7,16 @@ def main():
 
 
 def validate(ip):
-    matches =  re.search(r"^([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)$", ip, re.IGNORECASE)
-    if matches:
-        list_of_matches = []
-        for i in range(4):
-            list_of_matches.append(int(matches.group(i+1)))
-        print(list_of_matches)
-        for num in list_of_matches:
-            if num < 0 or num > 255:
+    if re.search(r"^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$", ip, re.IGNORECASE):
+        nums = ip.split(".")
+        for num in nums:
+            if int(num) < 0 or int(num) > 255:
                 return False
             else:
                 return True
-        return True
-    return False
+    else:
+        return False
+
 
 
 
