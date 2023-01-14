eab6da053b4587d105d7dab92912b77487c0ee43
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-14 21:10:22 +0000

    Sat, Jan 14, 2023, 9:10 PM +00:00

--- working/working.py
+++ working/working.py
@@ -31,17 +31,17 @@ def check_valid_time(t):
 
 def convert_valid_time(t):
     am_or_pm = t[-2:]
-    print(t)
     time = t.split(" ")[0]
     if ":" in time:
         hr, min = time.split(":")
     #     if int(hr) > 12 or int(hr) < 1 or int(min) > 59 or int(min) < 0:
     #         return False
     else:
-
+        hr = time
     #     if int(time) > 12 or int(time) < 1:
     #         return False
     # return True
+    print(hr)
 
 
 if __name__ == "__main__":
