acc8acdf5c1bda2485dfc49f51fee7974238deb0
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-14 19:45:25 +0000

    Sat, Jan 14, 2023, 7:45 PM +00:00

--- watch/watch.py
+++ watch/watch.py
@@ -8,7 +8,7 @@ def main():
 
 def parse(s):
     if matches :=  re.search(r'^<iframe.+src="(https?://(www)?\.youtube\.com/embed/[a-z0-9]+)".*></iframe>$', s, re.IGNORECASE):
-        print(matches.group)
+        url = re.sub(r"", matches.group(1))
 
 
 
