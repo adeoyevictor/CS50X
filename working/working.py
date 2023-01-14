3cee4594560168e870c1202a9a9c7684b1a41ac8
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-14 20:30:23 +0000

    Sat, Jan 14, 2023, 8:30 PM +00:00

--- working/working.py
+++ working/working.py
@@ -10,6 +10,8 @@ def convert(s):
     if matches := re.search(r"^([0-9]{1,2}:?([0-9]{2})? (AM|PM)) to ([0-9]{1,2}:?([0-9]{2})? (AM|PM))$", s):
         print(matches.group(1))
         print(matches.group(2))
+        print(matches.group(3))
+        print(matches.group(4))
 
 
 if __name__ == "__main__":
