355e563cc8926c370f407a770a83af37bcf95d0c
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-14 17:37:44 +0000

    Sat, Jan 14, 2023, 5:37 PM +00:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -7,12 +7,10 @@ def main():
 
 
 def validate(ip):
-    matches = re.search(r"^([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)\.$", ip, re.IGNORECASE)
-    if matches:
-        print(matches.group(1))
-        print(matches.group(2))
-        print(matches.group(3))
-        print(matches.group(4))
+    if re.search(r"^([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)\.$", ip, re.IGNORECASE):
+        print("True")
+    else:
+        print("False")
 
 
 
