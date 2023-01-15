9fca1e9e9c1c4e65cc8ba6fe091f667061944801
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-15 16:29:54 +0000

    Sun, Jan 15, 2023, 4:29 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -7,4 +7,4 @@ pdf = FPDF()
 pdf.add_page()
 
 pdf.set_font('helvetica', 'B', 16)
-pdf.cell(40, 10, 'CS50 Shirtificate', 1)
+pdf.cell(60, 10, 'CS50 Shirtificate.', new_x="LMARGIN", new_y="NEXT", align='C')
