ec465a54dff32ec57b57d9743b1a93554707c07a
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-15 13:13:29 +0000

    Sun, Jan 15, 2023, 1:13 PM +00:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,13 +1,13 @@
 from datetime import date
+import re
 
 
 def main():
-    dob = input("Date of birth: ")
-    ...
+    dob = check_format(input("Date of birth: "))
 
 
-...
-
+def check_format(d):
+    return re.search(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$")
 
 if __name__ == "__main__":
     main()
