b6ac10af913ca5bf816346cd7975d3e66cd5fb00
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-15 19:13:45 +0000

    Sun, Jan 15, 2023, 7:13 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -3,7 +3,9 @@ from fpdf import FPDF
 name = input("Name: ")
 
 pdf = FPDF()
+
 pdf.add_page()
+
 pdf.set_font("helvetica", "B", 30)
 pdf.cell(100, 40, "CS50 Shirtificate", align='C')
 pdf.output("shirtificate.pdf")
