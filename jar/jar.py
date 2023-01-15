6d043c80389e87ae4f201b89a214e3637f90df95
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-15 14:27:11 +0000

    Sun, Jan 15, 2023, 2:27 PM +00:00

--- jar/jar.py
+++ jar/jar.py
@@ -3,13 +3,17 @@ class Jar:
         if capacity < 0:
             raise ValueError
         self.capacity = capacity
+        self.n = 0
 
 
     def __str__(self):
-        return "🍪" * self.capacity
+        return "🍪" * self.n
 
     def deposit(self, n):
-        ...
+        if (self.n + n) > capacity:
+            raise ValueError("Jar is full")
+        else:
+            self.n += n
 
     def withdraw(self, n):
         ...
