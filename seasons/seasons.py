007d8a5d78f06c82d3202a6731ba04edc33e6694
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-15 13:15:18 +0000

    Sun, Jan 15, 2023, 1:15 PM +00:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -3,11 +3,14 @@ import re
 
 
 def main():
-    dob = check_format(input("Date of birth: "))
+    if check_format(input("Date of birth: ")):
+        print("Valid")
+    else:
+        print("Invalid")
 
 
 def check_format(d):
-    return re.search(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$")
+    return re.search(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}$", d, re.IGNORECASE)
 
 if __name__ == "__main__":
     main()
