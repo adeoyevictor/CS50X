b88eb934351550803e1143844e71acc008dd279c
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-15 19:37:58 +0000

    Sun, Jan 15, 2023, 7:37 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -2,16 +2,7 @@ from fpdf import FPDF
 
 name = input("Name: ")
 
-class PDF(FPDF):
-    def header(self):
-        self.set_font("helvetica", "B", 18)
-        self.cell(80)
-        self.cell(30, 10, "CS50 Shirtificate", align="C")
-        self.ln(20)
-
-
-# Instantiation of inherited class
-pdf = PDF()
+pdf = FPDF()
 pdf.add_page()
-pdf.image("shirtificate.png")
+
 pdf.output("shirtificate.pdf")
