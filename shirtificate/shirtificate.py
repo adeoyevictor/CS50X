bd2d265d35bf45e21af5d94b40e4cbc4eab30bb8
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-15 16:46:48 +0000

    Sun, Jan 15, 2023, 4:46 PM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -8,4 +8,6 @@ pdf.add_page()
 
 pdf.set_font('helvetica', 'B', 16)
 pdf.cell(60, 10, 'CS50 Shirtificate.', new_x="LMARGIN", new_y="NEXT", align='C')
+
+pdf.image("shirtificate.png", )
 pdf.output("shirtificate.pdf")
