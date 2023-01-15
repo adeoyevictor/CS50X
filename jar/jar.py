2a17167c5d5f02f9a6018dd9aa1403da8ad42d30
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2023-01-15 14:23:35 +0000

    Sun, Jan 15, 2023, 2:23 PM +00:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,10 +1,12 @@
 class Jar:
     def __init__(self, capacity=12):
+        if capacity < 0:
+            raise ValueError
         self.capacity = capacity
-        ...
+
 
     def __str__(self):
-        ...
+        return "🍪" * self.capacity
 
     def deposit(self, n):
         ...
